a36cdb2a7b3726ea7324fef56e5f60f2ed993754
Ritik <ritikpradhan20@gmail.com>
2023-10-26 17:35:25 +0530

    Initial commit

---
+++ Instabot.py
@@ -0,0 +1,13 @@
+from instabot import Bot
+bot = Bot()
+bot.login(username="ritikpradhan__",password="******")# Enter the right password
+# bot.follow('ritikpradhan_')
+# bot.upload_photo("C:/Users/aman pradhan/OneDrive/Pictures/landscape.jpg",caption="I love my Bike")
+# bot.unfollow("ritikpradhan_")
+# bot.send_message("I Love Bike",["ritikpradhan_"])
+# followers = bot.get_user_followers("ritikpradhan_")
+# for follower in followers:
+#     print(bot.get_user_info(follower))
+# following = bot.get_user_following("ritikpradhan_")
+# for Following in following:
+#     print((bot.get_user_info(following)))
